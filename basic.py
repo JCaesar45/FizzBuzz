c0d86a2e3c2c773539f40baf2f898a59eaba2842
Jordan Leturgez <jordanleturgez@gmail.com>
2026-03-14 19:57:48 -0400

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,17 @@
+def fizz_buzz():
+    result = []
+    
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+    
+    return result
+
+# Test
+print(fizz_buzz())
